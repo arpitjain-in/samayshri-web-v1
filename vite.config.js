8cf466085e6d6b225207afe0e91fabb22c735a0e
Arpit Jain <jainarpit@hotmail.com>
2026-02-16 21:30:14 -0600

    Carousel images changes

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     sourcemap: false,
     rollupOptions: {
       output: {
         manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'animation-vendor': ['framer-motion'],
-          'firebase-vendor': ['firebase/app', 'firebase/firestore'],
+          "react-vendor": ["react", "react-dom", "react-router-dom"],
+          "animation-vendor": ["framer-motion"],
+          "firebase-vendor": ["firebase/app", "firebase/firestore"],
         },
       },
     },
+    server: {
+      host: "192.168.0.186",
+      port: 5173, // or '0.0.0.0' to listen on all addresses
+    },
   },
-})
+});
