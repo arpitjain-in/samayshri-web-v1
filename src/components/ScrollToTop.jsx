5527c0fc142831c944e67ef602013d7c087111f0
Arpit Jain <jainarpit@hotmail.com>
2026-03-18 23:14:15 -0500

    firebase settings

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,15 +1,21 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { trackPageView } from '../utils/analytics';
 
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
+    // Scroll to top
     window.scrollTo({
       top: 0,
       left: 0,
       behavior: 'instant',
     });
+
+    // Track page view
+    const pageTitle = document.title || pathname;
+    trackPageView(pathname, pageTitle);
   }, [pathname]);
 
   return null;
